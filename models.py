07f02f753d50648bc4578207ba5e626506d7d9cb
ka-few <njorovista@gmail.com>
2025-08-26 08:29:38 +0300

    create tables

---
+++ models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Integer, Float, String, DateTime, Text, create_engine, Column, ForeignKey, Boolean
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+
+Base = declarative_base()
+
+class Tenant(Base):
+    __tablename__  = "tenants"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(70), nullable=False)
+    gender = Column(String(7))
+    email = Column(String(60))
+    
+class Property(Base):
+    __tablename__="properties"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable=False)
+    location = Column(String(50), nullable=False)
+    house_num = Column(String(7))
+    rent_amount = Column(Float(10,4))
+    
+class Rental_contract(Base):
+    __tablename__="rental_contracts"
+    id = Column(Integer, primary_key=True)
+    tenant_id = Column(Integer, ForeignKey("tenants.id"))
+    property_id = Column(Integer, ForeignKey("properties.id"))
+    start_date = Column(DateTime)
+    
+engine = create_engine("sqlite:///rentals.db")
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+Session = Session()
